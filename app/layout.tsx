5259f137345fe42abba8075fd4e4c10de9902824
Jim Lin <jhlin5083@gmail.com>
2025-02-25 18:27:32 +0800

    feat: Chapter 17 completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | NextJS Tutorial/Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official dashboard for Acme Inc.',
+  metadataBase: new URL('https://nextjs-tutorial-peach.vercel.app'),
+}
 export default function RootLayout({
   children,
 }: {
